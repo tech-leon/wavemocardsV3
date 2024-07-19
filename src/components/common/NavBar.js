ce7ee1b24cbc0bb8acc6f5784f1e3b0ad21bd704
Leon <107266694+leonfuns@users.noreply.github.com>
2024-07-20 02:43:27 +0800

    fix header.

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
   const { t } = useTranslation();
   return (
     <>
-      <nav className="flex flex-wrap items-center lg:w-1/3 space-x-2 lg:space-x-5">
+      <nav className="flex flex-wrap items-center md:w-1/3 space-x-2 lg:space-x-5">
         <Link to="/">{t("nav.home")}</Link>
         {/* <Link to="/about">{t("nav.about")}</Link> */}
         <Link to="/emotions">{t("nav.emotions")}</Link>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,6 +4,7 @@ import NavBar from "../common/NavBar";
 import ChangeLng from "../common/ChangeLng";
 import DarkModeSwitch from "../common/DarkModeSwitch";
 import UserIcon from "../common/UserIcon";
+import { Link } from "react-router-dom";
 
 function Header() {
   const { t } = useTranslation();
@@ -11,22 +12,21 @@ function Header() {
   return (
     <>
       <header className="sticky top-0 text-gray-600 bg-gray-100 dark:bg-gray-800 dark:text-white shadow-md z-50">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <div className="container mx-auto flex p-5 flex-col md:flex-row items-center ">
           <NavBar />
-          <a
-            href="/"
-            className="flex order-first lg:order-none lg:w-1/3 title-font font-medium items-center lg:items-center lg:justify-center mb-4 md:mb-0"
-          >
-            <img
-              className="size-9"
-              src="/images/logos/logo.png"
-              alt="logo"
-            ></img>
-            <span className="ml-3 text-3xl text-[#3c9daeff]">
-              {t("nav.app")}
-            </span>
-          </a>
-          <div className="lg:w-1/3 inline-flex lg:justify-end ml-5 lg:ml-0">
+          <div className="flex md:w-1/3 title-font font-medium items-center justify-center md:self-center my-4	md:my-0">
+            <Link to="/" className="w-full flex items-center justify-center justify-items-end	hover:no-underline">
+              <img
+                className="size-9"
+                src="/images/logos/logo.png"
+                alt="logo"
+              ></img>
+              <span className="ml-3 text-3xl	 text-[#3c9daeff]">
+                {t("nav.app")}
+              </span>
+            </Link>
+          </div>
+          <div className="md:w-1/3 inline-flex md:justify-end ml-5 md:ml-0">
             <UserIcon />
             <ChangeLng />
             <DarkModeSwitch />
