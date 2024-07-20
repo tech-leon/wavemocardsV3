7dd3bc2a20664072cc4495efef7626d9e7881dfc
Leon <107266694+leonfuns@users.noreply.github.com>
2024-07-20 14:01:43 +0800

    fix env issue

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import dotenv from 'dotenv';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+dotenv.config();
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDU1ilUni2AkxEFHeFzoTrkc2ZPXuGXURg",
-  authDomain: "wavemocards.firebaseapp.com",
-  projectId: "wavemocards",
-  storageBucket: "wavemocards.appspot.com",
-  messagingSenderId: "327511843796",
-  appId: "1:327511843796:web:604c280ebee22849546c41"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
