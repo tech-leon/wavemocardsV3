baeb90bfcac190636f4686cb1843737f62c2dbdc
Leon <107266694+leonfuns@users.noreply.github.com>
2024-07-20 14:08:11 +0800

    remove commend

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import dotenv from 'dotenv';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 dotenv.config();
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -13,8 +9,5 @@ const firebaseConfig = {
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export default app;
