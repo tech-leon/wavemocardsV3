bd2172235bf2c629d99d597292f11ff7df0ded1b
Leon <107266694+leonfuns@users.noreply.github.com>
2024-07-28 00:03:19 +0800

    usericon dropdown menu

--- src/components/common/UserIcon.js
+++ src/components/common/UserIcon.js
@@ -1,16 +1,53 @@
+import React, { useState, useRef, useEffect } from "react";
 import { UserCircleIcon } from "@heroicons/react/16/solid";
-import React from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 function UserIcon() {
+  const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
-      <Link
-        to="/user/profile"
-        className="hover:fill-[#3c9daeff] dark:fill-gray-100 flex items-center justify-center"
-      >
-        <UserCircleIcon className="w-8 h-8" />
-      </Link>
+      <div className="relative max-w-fit" ref={dropdownRef}>
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center hover:text-[#3c9daeff] justify-center space-x-1 focus:outline-none h-9 px-2 py-1"
+        >
+          <UserCircleIcon className="w-8 h-8" />
+        </button>
+        {isOpen && (
+          <div className="absolute right-0 mt-2 py-2 w-32 bg-white dark:bg-gray-800 rounded-md shadow-xl z-20">
+            <Link
+              to="/user/profile"
+              className="block hover:text-[#3c9daeff] hover:dark:bg-white hover:bg-gray-800 px-4 py-2 text-sm w-full text-left"
+            >
+              {t("nav.profile")}
+            </Link>
+            <Link
+              to="/user/myrecord"
+              className="block hover:text-[#3c9daeff] hover:dark:bg-white hover:bg-gray-800 px-4 py-2 text-sm w-full text-left"
+            >
+              {t("nav.myrecord")}
+            </Link>
+          </div>
+        )}
+      </div>
     </>
   );
 }
