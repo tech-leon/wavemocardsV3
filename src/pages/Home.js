d52b4ecc9a2879cb03173ce87fc92798afad893e
Leon <107266694+leonfuns@users.noreply.github.com>
2024-07-28 19:08:48 +0800

    start integrate

--- src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-
-function Home() {
-  const { t } = useTranslation();
-  return <h1>{t('pages.home.title')}</h1>;
-}
-
-export default Home;
