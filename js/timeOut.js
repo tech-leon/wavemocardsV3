f578dd80e80cee45bdfb50fa7e5f36c317bfc677
Leon <107266694+tech-leon@users.noreply.github.com>
2025-01-13 11:18:01 +0800

    Updata timeout to a week.

--- js/timeOut.js
+++ js/timeOut.js
@@ -4,7 +4,7 @@ let objStartTime = JSON.parse(localStorage.getItem("startTime"));
 let startTime = new Date(`${objStartTime.year}-${objStartTime.month}-${objStartTime.day}`);
 startTime.setHours(objStartTime.hour, objStartTime.min, objStartTime.sec);
 let nowTime = new Date();
-let costTime = Math.floor((nowTime - startTime) / (1000 * 60 * 60)); // 以小時計算
+let costTime = Math.floor((nowTime - startTime) / (1000 * 60 * 60 * 24));
 if(costTime>=7){
     localStorage.clear();
     btnTimeOut.click();
